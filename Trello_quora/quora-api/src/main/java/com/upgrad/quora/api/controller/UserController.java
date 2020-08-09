dba1b259530b00d738ec411ad8826ac4066ded26
Srinivas BV <srinivasbvsharma@gmail.com>
2020-08-09 17:04:16 +0530

    Added the SigninController Class

--- Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.upgrad.quora.api.controller;
 
 import com.upgrad.quora.api.model.SigninResponse;
-import com.upgrad.quora.service.business.SignupBusinessSevice;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.SignupBusinessSevice;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Base64;
 import java.util.UUID;
 
 @RestController
@@ -47,6 +50,19 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.POST, path = "/user/sigin",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SigninResponse> sigin(@RequestHeader("authorization") final String authorization){
+
+        byte[] decode = Base64.getDecoder().decode(authorization.split("Basic ")[1]);
+        String decodedText = new String(decode);
+        String[] decodedArray = decodedText.split(":");
+
+        UserAuthEntity userAuthToken = authenticationService.authenticate(decodedArray[0],decodedArray[1]);
+
+        UserEntity user = userAuthToken.getUser();
+
+        SigninResponse signinResponse = new SigninResponse().id(UUID.fromString(user.getUuid())).message("SIGNED IN SUCCESSFULLY");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access-token", userAuthToken.getAccessToken());
+        return new ResponseEntity<SigninResponse>(signinResponse,headers, HttpStatus.OK);
 
     }
 
